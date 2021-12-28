875e7cf93afc8166f7aacc25d12e7ec5cf430954
Zach-Sisson-1 <zach.r.sisson@gmail.com>
2021-12-27 19:38:04 -0800

    uploading all cleaned notebooks

---
+++ Edge_Prediction/PredictedGraphCreation.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import argparse
+import pandas as pd
+import networkx as nx
+import numpy as np
+import pickle5 as pkl
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+# Define arguments
+def parse():
+	arg = argparse.ArgumentParser()
+	arg.add_argument('-f', '--file', help='Pickle file input containing predicted edges', required=True)
+	arg.add_argument('-g', '--graph', help='Graph file to add predictions to.', required=True)
+	arg.add_argument('-o', '--output', help='File path/name of new graph with added predictions.', required=True)
+	return arg.parse_args()
+
+# grab args
+args = parse()
+
+
+# load pickle 
+df = pd.read_pickle(args.pickle)
+
+# drop non connections
+nonZeroDf = df[df.predictions == np.array(1.0)]
+
+
+# now pull out the edges
+edge_nonZeroDf = list(nonZeroDf.index)
+
+# Load input graph
+with open(args.graph, 'rb') as file:
+	healthy_G = pkl.load(file) 
+
+print(healthy_G)
+
+# pull out the edges from the healthy Graph
+G_edges = nx.to_pandas_edgelist(healthy_G)
+
+print(G_edges)
+
+# compare edge_nonZeroDf to G_edges
+print('###############################')
+print('Calculating New Predicted Edges')
+print('###############################')
+not_in_G_edges = list()
+for edge in tqdm(edge_nonZeroDf):
+	tuple_list = [(p1,p2) for p1,p2 in zip(G_edges.source, G_edges.target)]
+	if edge not in tuple_list:
+		e1, e2 =  edge
+		not_in_G_edges.append((e1, e2, {'color': 'red'}))
+
+print('Edges Calculated') 
+
+numHealthy = len(healthy_G.edges())
+healthy_G.add_edges_from(not_in_G_edges)
+
+
+print('Old graph has {} edges and the new graph now has {} edges'.format(numHealthy, len(healthy_G.edges())))
+
+
+# update edge colors (red is predicted, black is original)
+for edge in healthy_G.edges(data=True):
+	if edge[2] == dict():
+		edge[2]['color'] = 'black'
+
+
+nx.write_gpickle(healthy_G, path='{}.pkl'.format(args.output))
